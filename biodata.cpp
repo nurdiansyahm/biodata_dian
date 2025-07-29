3fe280f07c52569773a2266059e1a18fb82088c3
nurdiansyahm <syahnurdans@gmail.com>
2025-07-29 12:21:21 +0000

    upload biodata

---
+++ biodata.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Data biodata
+        string nama = "Muhamad Nurdiansyah";
+            string nim = "123456789";
+                string jurusan = "Teknik Informatika";
+                    string universitas = "Universitas Contoh";
+                        int umur = 21;
+                            string email = "nurdiansyah@example.com";
+                                string hobi = "Ngoding, Membaca, dan Traveling";
+
+                                    // Menampilkan data
+                                        cout << "===========================" << endl;
+                                            cout << "        BIODATA DIRI       " << endl;
+                                                cout << "===========================" << endl;
+                                                    cout << "Nama         : " << nama << endl;
+                                                        cout << "NIM          : " << nim << endl;
+                                                            cout << "Jurusan      : " << jurusan << endl;
+                                                                cout << "Universitas  : " << universitas << endl;
+                                                                    cout << "Umur         : " << umur << " tahun" << endl;
+                                                                        cout << "Email        : " << email << endl;
+                                                                            cout << "Hobi         : " << hobi << endl;
+                                                                                cout << "===========================" << endl;
+
+                                                                                    return 0;
+                                                                                    }
